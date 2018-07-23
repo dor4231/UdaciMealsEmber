4b31cf085917515d16bbe80c11bb6cdacea0161a
Dor Bar <dor4231@gmail.com>
2018-07-23 23:10:21 +0300

    feat: Can add each meal to a day in the week

--- app/components/menu-item.js
+++ app/components/menu-item.js
@@ -1,6 +1,17 @@
 import Component from '@ember/component';
+import { inject } from '@ember/service';
 
 export default Component.extend({
+    orderManager: inject.service('order-manager'),
     tagName: 'li',
-    classNames: ['menu-item']
+    classNames: ['menu-item'],
+    mealCategory: 'breakfast',
+    actions: {
+        changeCategory(category) {
+            this.set('mealCategory', category)
+        },
+        chooseItem(menuItemName) {
+            this.get('orderManager').chooseMenuItem(this.get('mealCategory'), menuItemName)
+    }
+}
 });
